c7ab211861c4d2bcd8224b403f9a8bd5bd6f8942
Xheni Xhelilaj <73161981+xhenixhelilaj@users.noreply.github.com>
2021-05-18 13:35:34 +0200

    build html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Weather from "./Weather"
+import "./Weather.css"
 
 function App() {
   return (
     <div className="App">
-     <h1>Weather App</h1>
+     <Weather />
      <footer>
        This project is created by Xheni Xhelilaj and is 
        <a href="https://github.com/xhenixhelilaj/react-weather-project" target="_blank"> open-sourced </a> 
---
+++ src/Weather.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+export default function Weather(){
+    return(
+        <div className="Weather">
+            <form>
+                <div className="row">
+                   <div className="col-9">
+                     <input type="search" placeholder="Enter a city" autoFocus="on" />
+                </div>
+                <div className="col-3">
+                     <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                </div> 
+                </div>  
+            </form>
+            <h1>Zurich</h1>
+            <ul>
+                <li>
+                    Monday 11:40
+                </li>
+                <li>
+                    Cloudy
+                </li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="cloudy"/>
+                    6°C
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Humidity:77%</li>
+                        <li>Wind:14km/h</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+
+}
